01ad8705a168b7bca78f32ea1fe19e01348cd2e7
Five So Beş <89789101+Bes-js@users.noreply.github.com>
2024-03-07 22:17:02 +0300

    Update index.d.ts

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,8 +1,34 @@
-import { Knex } from 'knex';
+import { Sequelize } from 'sequelize';
 import Model from './src/model';
 
-    declare const db: Knex<any, unknown>;
+    declare const db: Sequelize;
 
+    /**
+     * @type {{DeleteOne: string, DeleteMany: string, UpdateOne: string, UpdateMany: string, InsertOne: string, InsertMany: string, FindOne: string, Find: string, DatabaseConnected: string, DatabaseConnectionFailed: string, BackupCreated: string, UnexpectedError: string}}
+     * @readonly
+     * @enum {string}
+    declare const EventsTypes: {
+    DeleteOne: "deleteOne",
+    DeleteMany: "deleteMany",
+    UpdateOne: "updateOne",
+    UpdateMany: "updateMany",
+    InsertOne: "insertOne",
+    InsertMany: "insertMany",
+    FindOne: "findOne",
+    Find: "find",
+    DatabaseConnected: "databaseConnected",
+    DatabaseConnectionFailed: "databaseConnectionFailed",
+    BackupCreated: "backupCreated",
+    UnexpectedError: "unexpectedError",
+    };
+    */
+
+
+    
+    /**
+     * Represents the available types in the application.
+     * @returns {Types}
+     */
     declare const Types: {
         Number: "number",
         Float: "float",
@@ -13,10 +39,26 @@ import Model from './src/model';
         Array: "array",
     };
 
+    /**
+     * Represents a function that creates a schema object.
+     * @param values - An object representing the schema values.
+     * @returns A schema object.
+     */
     declare function Schema(values: Record<any, {type?:string,default?:any,required?:boolean}>|Record<any,any>): Record<any, {type?:string,default?:any,required?:boolean}>|Record<any,any>;
+
+    
+    /**
+     * Retrieves information about the database.
+     * @returns An object containing the following properties:
+     * - fileSize: The size of the database file.
+     * - lastModified: The date and time when the database was last modified.
+     * - createdAt: The date and time when the database was created.
+     * - uid: The unique identifier of the database.
+     * If the information is not available, it returns null.
+     */
     declare function DatabaseInformation():{fileSize:string, lastModified:string, createdAt:string, uid:number} | null;
 
-    export { 
+    export {
     db as Driver,
     Model,
     Schema,
