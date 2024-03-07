bb048c2387f97e39c857c0c5ac774ca305dc783b
Five So Beş <89789101+Bes-js@users.noreply.github.com>
2024-03-07 22:16:21 +0300

    Create events.js

---
+++ src/events.js
@@ -0,0 +1,11 @@
+
+
+/** 
+* @description This module is responsible for emitting events.
+* @module events
+* @deprecated
+* @todo
+const EventEmitter = require('events');
+const Events = new EventEmitter();
+module.exports = Events;
+*/
